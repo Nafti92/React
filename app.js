946254b69e175fe84e59d6bc373883cc565cc781
nafti ladhiri <ladhiri.nafti@gmail.com>
2023-02-04 14:52:21 +0100

    react day 3 pt4

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-class Field extends React.Component {
-  render () {
-    const {name, value, onChange, children} = this.props
+function Field ({name, value, onChange, children}) {
     return (
-      <div>
+      <div className="form-group">
         <label htmlFor={name}>{children}</label>
-        <input type="text" value={value} onChange={onChange} id={name} name={name}/>
+        <input type="text" value={value} onChange={onChange} id={name} name={name} className="form-control"/>
       </div>
     )
   }
-}
 
+  function Checkbox ({name, value, onChange, children}) {
+    return (
+      <div className="form-check">
+        <label htmlFor={name} className="form-check-label">{children}</label>
+        <input type="checkbox" value={value} onChange={onChange} id={name} name={name} className="form-check-input"/>
+      </div>
+    )
+  }
 
 
 class Home extends React.Component {
@@ -22,6 +27,7 @@ class Home extends React.Component {
       newsletter: false
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange (e) {
@@ -33,15 +39,29 @@ class Home extends React.Component {
     })
   }
 
+  handleSubmit (e) {
+    e.preventDefault()
+    const data = JSON.stringify(this.state)
+    this.setState ({
+      nom: '',
+      prenom: '',
+      newsletter: false
+    })
+  }
+
 
   render () {
     return (
-      <div className="container">
+      <form className="container" onSubmit={this.handleSubmit}>
         <Field name="nom" value={this.state.nom} onChange={this.handleChange}>name</Field>
         <Field name="prenom" value={this.state.prenom} onChange={this.handleChange}>firstname</Field>
+        <Checkbox name="newsletter" value={this.state.newsletter} onChange={this.handleChange}>S'abonner à la newsletter</Checkbox>
+        <div className="form-group">
+          <button className="btn btn-primary">Envoyer</button>
+        </div>
         {JSON.stringify(this.state)}
 
-      </div>
+      </form>
     )
   }
 }
